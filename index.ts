62dafddba10493c84a173f63526092f3d4559419
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-01-18 22:29:00 +0600

    feat: add websocketServer and send data to history

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { httpServer } from "./src/http_server/index";
+import { RawData, WebSocketServer } from 'ws';
 import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
@@ -8,4 +9,14 @@ httpServer.listen(HTTP_PORT);
 
 process.on('SIGINT', () => {
     httpServer.close(() => process.exit());
+});
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (data: RawData) => {
+      console.log('received: %s', data);
+
+      ws.send(data.toString());
+    });
 });
